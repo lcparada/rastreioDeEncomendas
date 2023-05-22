6eb868cce0b00779c727398be5cc30adb4fb4d07
lcparada <loparada@gmail.com>
2023-05-22 11:21:33 -0300

    chore: navigation types

---
+++ @types/navigation.d.ts
@@ -0,0 +1,14 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Welcome: undefined;
+      Register: undefined;
+      Home: undefined;
+      EditProfile: undefined;
+      InformationApp: undefined;
+      Profile: undefined;
+      Login: undefined;
+      AppRoutes: undefined;
+    }
+  }
+}
